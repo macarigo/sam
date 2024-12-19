acee886c5b2179bea38d010315ee9e0e019fe590
axel <ssaxel03@gmail.com>
2024-12-19 20:45:53 +0000

    fix: fixed api http requests. for the last time I hope please god hear me vai te foder

--- components/homeMap.jsx
+++ components/homeMap.jsx
@@ -35,7 +35,7 @@ const Map = ({ refresh, onClick, getLocation, setGetLocation, setMapClick }) => 
 
     const fetchMarkers = async () => {
         try {
-            const response = await fetch('https://18ae-13-60-190-47.ngrok-free.app/api/locations'); // Replace with your API URL
+            const response = await fetch('https://18ae-13-60-190-47.ngrok-free.app/api/occurencies'); // Replace with your API URL
             if (!response.ok) throw new Error('Failed to fetch locations');
             const data = await response.json();
             setMarkers(data); // Update state with fetched data
